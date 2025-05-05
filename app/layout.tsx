aaad4621f11981385e40517f0d49822824f13014
viihunnid <viihunnid@icloud.com>
2025-05-05 22:50:50 +0200

    Used the built-in types

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="public/favicon.png" type="image/png" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
       </head>
       <body className={inter.className}>
         <DottedBackground pattern="dots">
