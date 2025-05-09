4728739e04b390bea7f012bbf4467b011cceb99e
Vii-Hunnid <viihunid@icloud.com>
2025-05-09 13:10:26 +0200

    Add this script to your website's <head> section:

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,13 +24,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.png" type="image/png" />
+        <script src="https://www.sitetooling.space/track.js" data-site-id="6f77ee37-f5b7-4712-b63d-afb89661ab01" async defer></script>
+        <script src="https://www.sitetooling.space/track.js" data-website-id="be26c8ab-6eca-453a-995e-a1b06fcefd95" async defer></script>
+
+        <noscript>
+          <img src="https://sitetooling.space/noscript.gif?6f77ee37-f5b7-4712-b63d-afb89661ab01" alt="sitetooling.space old" />
+        </noscript><noscript>
+          <img src="https://sitetooling.space/noscript.gif?be26c8ab-6eca-453a-995e-a1b06fcefd95" alt="sitetooling.space new" />
+        </noscript>
       </head>
       <body className={inter.className}>
         {/* Apple Music Floating Embed */}
 
-        <script async defer src="https://sitetooling.space/track.js" data-site-id="pofs0dkge0bju6lbc40xz"></script>
-        <script src="https://www.sitetooling.space/track.js" data-website-id="be26c8ab-6eca-453a-995e-a1b06fcefd95" async defer></script>
         <AppleMusicEmbed />
+
         <Analytics/>
         <DottedBackground pattern="dots">
           <main className="container mx-auto px-4 py-8">
