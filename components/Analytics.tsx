b4c965766d6ee5b15a5152d6727b425b0fd8f6a5
Vii-Hunnid <98171862+Vii-Hunnid@users.noreply.github.com>
2025-05-28 16:32:58 +0200

    Update Analytics.tsx
    Updated the data-token 3

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -6,7 +6,21 @@ import Script from 'next/script'
 export default function Analytics() {
   return (
     <>      
-      <script src="https://www.sitetooling.space/analytics.js" data-token="469ff776-6c3e-4767-9b64-3a9eb7378eae" data-element="site-analytics"></script>
+      <Script
+        src="https://sitetooling.space/track.js"
+        data-token="469ff776-6c3e-4767-9b64-3a9eb7378eae"
+        strategy="afterInteractive"
+      />
+      
+      <Script
+        src="https://sitetooling.space/analytics.js"
+        data-token="469ff776-6c3e-4767-9b64-3a9eb7378eae"
+        data-element="site-analytics"
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log('Analytics script loaded successfully')
+        }}
+      />
       
       {/* The div where analytics will be rendered */}
       <div id="site-analytics"></div>
