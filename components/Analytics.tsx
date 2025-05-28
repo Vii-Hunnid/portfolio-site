ebd9650658ff9527ba36fdc0a183a94fb1af7fa5
Vii-Hunnid <98171862+Vii-Hunnid@users.noreply.github.com>
2025-05-28 16:28:51 +0200

    Update Analytics.tsx
    Updated the data-token 2

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -6,10 +6,6 @@ import Script from 'next/script'
 export default function Analytics() {
   return (
     <>      
-      <noscript>
-      <img src="https://www.sitetooling.space/noscript.gif?token=469ff776-6c3e-4767-9b64-3a9eb7378eae" alt="sitetooling.space" />
-      </noscript>
-
       <script src="https://www.sitetooling.space/analytics.js" data-token="469ff776-6c3e-4767-9b64-3a9eb7378eae" data-element="site-analytics"></script>
       
       {/* The div where analytics will be rendered */}
