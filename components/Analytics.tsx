ae8d5c1de5569c4fd69f9b10885783cbaf611a05
Vii-Hunnid <98171862+Vii-Hunnid@users.noreply.github.com>
2025-07-09 13:07:54 +0200

    Update Analytics.tsx

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -9,14 +9,10 @@ export default function Analytics() {
       <Script
         src="https://sitetooling.space/analytics.js"
         data-token="fe326de0-dda8-4c9d-b0e2-6089a93c5950"
-        data-theme="inherit"
-        data-background="transparent"
         data-controls="true"
         data-style="compact"
         data-element="site-analytics"
-        data-show-logo="true"
         data-show-header="true"
-        strategy="afterInteractive"
         />
       
       {/* The div where analytics will be rendered */}
