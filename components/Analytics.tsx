5c48e3544de8ec38718c41d6cf58aae1a301b3fb
Vii-Hunnid <98171862+Vii-Hunnid@users.noreply.github.com>
2025-08-05 11:31:16 +0200

    https://www.sitetooling.space/session-replay.js

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -11,6 +11,13 @@ export default function Analytics() {
         data-token="6f77ee37-f5b7-4712-b63d-afb89661ab01"
         strategy="afterInteractive"
       />
+
+       <Script 
+        async 
+        defer 
+        src="https://www.sitetooling.space/session-replay.js" 
+        data-token="fe326de0-dda8-4c9d-b0e2-6089a93c5950"
+      /> 
       
       <Script
         src="https://sitetooling.space/analytics.js"
